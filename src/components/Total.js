f009fbadeb43aff40412165d9c35eeb9d9b5943f
sebagarayco <sebagarayco@gmail.com>
2022-09-19 14:31:45 -0300

    Add: Form Cliente

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,13 +1,24 @@
 import './Total.css'
 import { useSelector, useDispatch } from 'react-redux'
 import { clearCart } from '../redux/cartSlice'
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
 
 function Total() {
   const dispatch = useDispatch()
   const cart = useSelector((state) => state.cart)
 
-  const pagar = () => {
-    alert('El state esta asi: ' + JSON.stringify(cart))
+  const clientData = ['Nombre', 'Apellido', 'Telefono', 'Documento', 'Domicilio']
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = evt => {
+    console.log('El ' + evt.target.id + ' es ' + evt.target.value)
+    const name = evt.target.id;
+    const value = evt.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+    console.log(inputs)
   }
 
   const getTotal = () => {
@@ -20,38 +31,67 @@ function Total() {
     return { totalPrice, totalQuantity }
   }
 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Form submitted ✅' + JSON.stringify(event, inputs));
+  }
+
   return (
-    <div className="total">
-      <h2>Detalle de compra</h2>
-      {
-        cart.map(item => (
-          <div className="total__item" key={item.id}>
-            <div className="total__item__quantity">
-              {item.quantity} x {item.title}
+    <form onSubmit={handleSubmit}>
+      <div className="total">
+        <div className="cliente">
+          <h2>Cliente</h2>
+          <Box
+            sx={{ '& .MuiTextField-root': { m: 1 }, }}
+            noValidate
+            autoComplete="off"
+          >
+            <div>
+              {
+                clientData.map(data => (
+                  <TextField key={data}
+                    fullWidth
+                    required
+                    id={data}
+                    label={data}
+                    onChange={handleChange}
+                  />
+                ))}
             </div>
-            <div className="total__item__price">
-              ${item.price * item.quantity}
+          </Box>
+        </div>
+        <h2>Detalle de compra</h2>
+        {
+          cart.map(item => (
+            <div className="total__item" key={item.id}>
+              <div className="total__item__quantity">
+                {item.quantity} x {item.title}
+              </div>
+              <div className="total__item__price">
+                ${item.price * item.quantity}
+              </div>
             </div>
+          ))
+        }
+        <div className="total__p__line">
+          <div className="total__p">
+            Total ({getTotal().totalQuantity} productos):
           </div>
-        ))
-      }
-      <div className="total__p__line">
-        <div className="total__p">
-          Total ({getTotal().totalQuantity} productos):
+          <div className="total__p__value">
+            ${getTotal().totalPrice}
+          </div>
         </div>
-        <div className="total__p__value">
-          ${getTotal().totalPrice}
+        <div className="total__buttons">
+          <button className="total__b__pagar" type="submit">
+            <strong>PAGAR</strong>
+          </button>
+          <button className="total__b__erase" onClick={() => dispatch(clearCart())}>
+            <strong>VACIAR</strong>
+          </button>
         </div>
       </div>
-      <div className="total__buttons">
-        <button className="total__b__pagar" onClick={pagar}>
-          <strong>PAGAR</strong>
-        </button>
-        <button className="total__b__erase" onClick={() => dispatch(clearCart())}>
-          <strong>VACIAR</strong>
-        </button>
-      </div>
-    </div>
+    </form>
   )
 }
 
